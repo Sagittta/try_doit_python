d2168009ec4af67a05e40b85e3121e5d22d2e6df
Sagittta <js1verj@gmail.com>
2019-10-24 23:01:19 +0900

    null_check()

--- join.py
+++ join.py
@@ -11,29 +11,44 @@ class Join:
 
         try:
             with conn.cursor() as curs:
+
+                j = Join(self.id, self.pwd)
+                result = j.null_check()
+
+                if result == 1:
+                    sql = "INSERT INTO profile(prof_num, id, pwd) VALUES (%s, %s, %s)"
+                    value = (0, self.id, self.pwd)
+                    curs.execute(sql, value)
+
+                    data = curs.fetchall()
+
+                    if not data:
+                        conn.commit()
+                        print("가입 완료")
+                    else:
+                        conn.rollback()
+                        print("가입 실패")
+
+        finally:
+            conn.close()
+
+    def null_check(self):
+        conn = pymysql.connect(host='localhost', user='root', password='mirim2', db='project', charset='utf8')
+
+        try:
+            with conn.cursor() as curs:
                 sql = "SELECT id FROM profile WHERE id='%s'" % self.id
                 curs.execute(sql)
                 data = curs.fetchall()
 
                 if data:
                     print("다른 아이디를 사용해주세요.")
+
+                    return 0
                 else:
                     print("사용할 수 있습니다.")
 
-                    with conn.cursor() as curs2:
-                        sql = "INSERT INTO profile(prof_num, id, pwd) VALUES (%s, %s, %s)"
-                        value = (0, self.id, self.pwd)
-                        curs2.execute(sql, value)
-
-                        data = curs.fetchall()
-
-                        if not data:
-                            conn.commit()
-                            print("가입 완료")
-                        else:
-                            conn.rollback()
-                            print("가입 실패")
-
+                    return 1
         finally:
             conn.close()
 
